79f7baaaa5c87c147c7d340f48219b362bcf12c8
lil-dev16 <olusegunkd2017@gmail.com>
2022-12-16 20:07:52 +0100

    types

---
+++ type.ts
@@ -0,0 +1,12 @@
+console.log('hello world');
+
+//TYPES
+let age = 15;
+let firstName = 'Segun';
+let student = true;
+
+if(student) {
+    //variables cannot be assigned another type:))
+    age = 'ree'; // this gives an error
+}
+

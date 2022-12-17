38adb812bafcfa6cb3c6262ef4329f1834bc9d64
lil-dev16 <olusegunkd2017@gmail.com>
2022-12-17 18:34:04 +0100

    function

--- function.ts
+++ function.ts
@@ -1,15 +1,23 @@
-const printVal = (name, val) => {
-    for(let i=0; i<=val; i++) {
-        console.log(`${name} is ${i} years old.`);
-    }
-}
+// const printVal = (name, val) => {
+//     for(let i=0; i<=val; i++) {
+//         console.log(`${name} is ${i} years old.`);
+//     }
+// }
 
-printVal('Segun', 8);
+// printVal('Segun', 8);
 
 //ARGUMENTS MAY NOT NEED TO BE USED WHEN CALLING THEM SO WE USE THE '?'
-function testr(status?: string) {
-    console.log(`I am ${status || 'not ready'}`)
+// function testr(status?: string) {
+//     console.log(`I am ${status || 'not ready'}`)
+// }
+
+// testr();
+// testr('ready')
+
+//default parameters
+const greetMaster = (status = 'Segun') =>{
+    console.log(`Good afternoon ${status}`);
+    
 }
 
-testr();
-testr('ready')
+greetMaster()
